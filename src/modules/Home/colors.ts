ffa4b298b5df88da4cd97744de749d429f95a92b
orrybaram <orrybaram@gmail.com>
2018-11-12 23:56:01 -0500

    generate all colors

--- src/modules/Home/colors.ts
+++ src/modules/Home/colors.ts
@@ -2,24 +2,40 @@ import colors from "../../colors";
 
 const { red, orange, yellow, green, blue, purple, neutral } = colors;
 
-const getBackgroundColors = () => {
-  const levels = [0, 10, 20, 30];
+const getColorVariants = (colorArray: string[], levels: number[]) => {
+  return colorArray.reduce((acc: string[], color: string) => {
+    const colorLevels = levels.map(level => {
+      return colors[color][level];
+    });
 
-  return Object.keys(colors)
-    .filter(x => x !== "neutral")
-    .reduce((acc: string[], color: string) => {
-
-      const colorLevels = levels.map(level => {
-        return colors[color][level];
-      })
-
-      return [...acc, ...colorLevels];
-    }, []);
+    return [...acc, ...colorLevels];
+  }, []);
 };
+
+const getBackgroundColors = () => {
+  return getColorVariants(
+    ["red", "orange", "yellow", "green", "blue", "purple"],
+    [10, 20]
+  );
+};
+
+const getBellyColors = () => {
+  return getColorVariants(
+    ["orange", "yellow", "neutral"],
+    [0]
+  );
+}
+
+const getCoatColors = () => {
+  return getColorVariants(
+    ["red", "orange", "yellow", "neutral"],
+    [0, 30, 40, 50, 60, 70, 80, 90, 100]
+  );
+}
 
 export const backgroundColors = getBackgroundColors();
 
-export const bellyColor = ["white"];
-export const coatColor = ["red", "brown", "black", "orange", "gray"];
-export const secondaryCoatColor = ["red", "brown", "black", "orange", "gray"];
-export const tertiaryCoatColor = ["red", "brown", "black", "orange", "gray"];
+export const bellyColor = getBellyColors();
+export const coatColor = getCoatColors();
+export const secondaryCoatColor = getCoatColors();
+export const tertiaryCoatColor = getCoatColors();
