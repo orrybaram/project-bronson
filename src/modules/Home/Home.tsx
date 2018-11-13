2b7617239f5d776d1b0e1e5d78f081dbe114c79d
orrybaram <orrybaram@gmail.com>
2018-11-12 22:30:39 -0500

    setup basics for generation

--- src/modules/Home/Home.tsx
+++ src/modules/Home/Home.tsx
@@ -1,20 +1,57 @@
-import * as React from 'react';
-import styled from 'react-emotion';
-import { Flex } from '@rebass/grid/emotion';
-import getGeneratedValues from './getGeneratedValues';
+import * as React from "react";
+import styled from "react-emotion";
+import getGeneratedValues from "./getGeneratedValues";
 
-const generatedValues = getGeneratedValues();
+const {
+  backgroundColor,
+  coatColor,
+  secondaryCoatColor,
+  tertiaryCoatColor,
+  bellyColor,
+  hasSpots
+} = getGeneratedValues();
 
-const Main: React.Component = styled(Flex)`
-  background: ${generatedValues.background};
+const Main = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: ${backgroundColor};
   padding: 20px;
   height: 100vh;
 `;
 
+const Dog = styled.div`
+  position: relative;
+  background-color: ${coatColor};
+  height: 50px;
+  width: 50px;
+`;
+
+const Belly = styled.div`
+  position: absolute;
+  background-color: ${bellyColor};
+  bottom: 0;
+  height: 30px;
+  width: 30px;
+  left: 50%;
+  transform: translateX(-50%);
+`;
+
+const Spots = styled.div`
+  background-color: ${secondaryCoatColor};
+  position: absolute;
+  left: 0;
+  height: 10px;
+  width: 10px;
+`;
+
 const Home = () => {
   return (
-    <Main justifyContent="center" alignItems="center">
-      <h1>:)</h1>
+    <Main>
+      <Dog>
+        <Belly />
+        {hasSpots && <Spots />}
+      </Dog>
     </Main>
   );
 };
--- src/modules/Home/getGeneratedValues.ts
+++ src/modules/Home/getGeneratedValues.ts
@@ -1,10 +1,26 @@
 const backgrounds = ["yellow", "red", "green", "purple", "pink", "cyan"];
+const coatColor = ["red", "brown", "black", "orange", "gray"];
+const secondaryCoatColor = ["red", "brown", "black", "orange", "gray"];
+const tertiaryCoatColor = ["red", "brown", "black", "orange", "gray"];
+const bellyColor = ["white"];
 
-const getRandomArrayValue = array => {
-  const randomIndex = Math.floor(Math.random() * array.length);
+const getRandomNumber = (range: number) => Math.floor(Math.random() * range);
+
+const getRandomArrayValue = (array: Array<string>) => {
+  const randomIndex = getRandomNumber(array.length);
+  console.log(randomIndex);
   return array[randomIndex];
 };
 
+const getProbability = (consequent: number) => {
+  return getRandomNumber(consequent) === 1;
+};
+
 export default () => ({
-  background: getRandomArrayValue(backgrounds)
+  backgroundColor: getRandomArrayValue(backgrounds),
+  coatColor: getRandomArrayValue(coatColor),
+  secondaryCoatColor: getRandomArrayValue(secondaryCoatColor),
+  tertiaryCoatColor: getRandomArrayValue(tertiaryCoatColor),
+  bellyColor: getRandomArrayValue(bellyColor),
+  hasSpots: getProbability(5)
 });
