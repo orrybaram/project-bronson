72c24bac1bd401ef16c274f847d2419940fb7125
orrybaram <orrybaram@gmail.com>
2018-11-15 01:51:34 -0500

    refactor generate script

--- src/modules/Home/getGeneratedValues.ts
+++ src/modules/Home/getGeneratedValues.ts
@@ -26,7 +26,9 @@ const getProbability = (consequent: number) => {
 
 const getBaseColor = () => baseCoat[getRandomArrayValue(baseCoat)];
 
-const generateValues = (baseColor: { hex: string; color: string }) => {
+const generateValues = () => {
+  const baseColor = getBaseColor()
+
   return {
     baseCoat: baseColor.hex,
     backgroundColor:
@@ -43,18 +45,11 @@ const generateValues = (baseColor: { hex: string; color: string }) => {
 };
 
 export default (): { data: ValuesType; meta: MetaType } => {
-  // TODO: steps
-  // 1. Roll D20 - if critical hit, Roll for special character, if not continue.
-  // 2. Roll for base color
-  // 3. Generate  underCoat, secondary colors, tertiary color, accent color, name
-
-  const baseColor = getBaseColor();
-
   const isCriticalHit = getProbability(20);
 
   const values = isCriticalHit
     ? craftedDogs[getRandomArrayValue(craftedDogs)]
-    : generateValues(baseColor);
+    : generateValues();
 
   const hash = createHash(values);
 
