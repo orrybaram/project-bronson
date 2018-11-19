9fd7fc5edac54b579faf35da075054413f8d5cf8
orrybaram <orrybaram@gmail.com>
2018-11-19 01:14:58 -0500

    gradient background

--- src/modules/Home/Home.styles.ts
+++ src/modules/Home/Home.styles.ts
@@ -1,12 +1,13 @@
 import styled from "react-emotion";
+import { lighten } from 'polished';
 
 export const Main = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  background: ${({ backgroundColor }: { backgroundColor: string }) =>
-    backgroundColor};
+  background-image: ${({ backgroundColor }: { backgroundColor: string }) =>
+    `radial-gradient(${lighten(0.05, backgroundColor)}, ${backgroundColor})`};
   padding: 20px;
   height: 100vh;
 `;
